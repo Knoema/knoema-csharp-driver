030aa44c3e951db253a4b4c93a5010286d595e15
Alexey Matyukhin <amatyukhin@knoema.com>
2017-09-19 12:39:15 +0500

    KN-33138

---
+++ Knoema.Client/Data/PivotRequestTimeSeriesAttributes.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Knoema.Data
+{
+	public class PivotRequestTimeSeriesAttributes
+	{
+		public string Name { get; set; }
+		public List<object> Values { get; set; }
+	}
+}
---
+++ Knoema.Client/Meta/TimeSeriesAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Knoema.Meta
+{
+    public class TimeSeriesAttribute
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public IEnumerable<string> AvailableValues { get; set; }
+    }
+}
