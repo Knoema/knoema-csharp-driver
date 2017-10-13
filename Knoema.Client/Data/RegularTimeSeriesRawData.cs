8bbb564e8404eb5dc7c8a40af0c235e375fc1e50
Alexey Matyukhin <amatyukhin@knoema.com>
2017-10-13 15:47:33 +0500

    KN-34141 KnoemaClient. Need to add mnemonics to the class RegularTimeSeriesRawData

--- Knoema.Client/Data/RegularTimeSeriesRawData.cs
+++ Knoema.Client/Data/RegularTimeSeriesRawData.cs
@@ -10,6 +10,7 @@ namespace Knoema.Data
 		public string Frequency { get; set; }
 		public string Unit { get; set; }
 		public float Scale { get; set; }
+		public string Mnemonics { get; set; }
 		public IEnumerable<object> Values { get; set; }
 	}
 }
