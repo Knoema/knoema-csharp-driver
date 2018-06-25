8e07e76a557b75e193a6711eceba5daf78271e94
BillSytin <billsytin@gmail.com>
2018-06-25 12:22:12 +0500

    KN-42465 Download from RedShift using Unload feature (#41)
    * KN-42465 Download from RedShift using Unload feature

---
+++ Knoema.Client/Data/UnloadResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Knoema.Data
+{
+	public class UnloadResponse
+	{
+		public int TaskKey { get; set; }
+	}
+}
---
+++ Knoema.Client/Meta/DatasetUnloadTaskResultData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Knoema.Meta
+{
+	public class DatasetUnloadTaskResultData
+	{
+		public class FileInfo
+		{
+			public string Name { get; set; }
+			public string Url { get; set; }
+			public long Length { get; set; }
+		}
+
+		private readonly List<FileInfo> _files;
+		public List<FileInfo> Files { get { return _files; } }
+
+		public DatasetUnloadTaskResultData()
+		{
+			_files = new List<FileInfo>();
+		}
+	}
+}
