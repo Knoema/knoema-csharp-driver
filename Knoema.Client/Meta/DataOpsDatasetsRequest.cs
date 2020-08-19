3d2a70d29958add3f819a5e2aef6d667c03471f8
de4ep <32199087+de4ep@users.noreply.github.com>
2020-08-19 11:29:05 +0500

    KN-79774 add meta/datasetsStats api (#63)
    Co-authored-by: cheplygin <cheplygin@knoema.com>

---
+++ Knoema.Client/Meta/DataOpsDatasetsRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Knoema.Meta
+{
+	public enum AutoFilter
+	{
+		NoCheck,
+		Auto,
+		Manual
+	}
+
+	public class DataOpsDatasetsRequest
+	{
+		public DateTime? ExpiredDatesetsDateFrom { get; set; }
+
+		public DateTime? ExpiredDatesetsDateTo { get; set; }
+
+		public bool? ExpectingUpdate { get; set; }
+
+		public IEnumerable<string> Ids { get; set; }
+
+		public AutoFilter? Auto { get; set; }
+
+		public IEnumerable<string> Statuses { get; set; }
+
+		public bool? IncludePrivate { get; set; }
+	}
+}
