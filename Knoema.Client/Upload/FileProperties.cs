1a065b7b66029ffbc416e39eb268f2912d8e7a63
cheplygin <cheplygin@knoema.com>
2021-01-22 17:49:29 +0500

    KN-81722 Add FileProperties.FileBucket to dataset upload object in Knoema.Client

--- Knoema.Client/Upload/FileProperties.cs
+++ Knoema.Client/Upload/FileProperties.cs
@@ -2,11 +2,18 @@
 
 namespace Knoema.Upload
 {
+	public enum FileBucket
+	{
+		Temp = 0,
+		Uploads = 1
+	}
+
 	public class FileProperties
 	{
 		public int Size { get; set; }
 		public string Name { get; set; }
 		public string Location { get; set; }
 		public string Type { get; set; }
+		public FileBucket? FileBucket { get; set; }
 	}
 }
